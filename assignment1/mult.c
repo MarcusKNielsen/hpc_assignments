9852ab599b23e360d09e01090b027ad206dd5996
Asta Rustad <asr017@bucknell.edu>
2025-01-08 11:32:25 +0100

    Added 6 functions for part 2

--- assignment1/mult.c
+++ assignment1/mult.c
@@ -21,6 +21,126 @@ void matmult_nat(int m,int n,int k,double *A,double *B,double *C){
     }
 }
 
+// Matrix multiplication with m as the outer loop, n as the next one and k 
+// as the inner loop. We already have this function (matmult_nat), so
+// we simply call this function.
+void matmult_mnk(int m,int n,int k,double *A,double *B,double *C){
+    matmult_nat(m, n, k, A, B, C);
+}
+
+void matmult_mkn(int m,int n,int k,double *A,double *B,double *C){
+    // Intialize C-matrix to 0.
+    for (int c_idx = 0; c_idx < m * n; c_idx++) {
+        C[c_idx] = 0;
+    }
+
+    // These two loops iterates over indexes of C: C[i,j]
+    for (int i=0; i<m; i++){
+        for (int l=0; l<k; l++){
+            int a_idx = i*k+l; // row-major indexing: "A[i,l] = A[a_idx]"
+
+            // This loop computes: C[i,j] = sum_l A[i,l]*B[l,j]
+            for (int j=0; j<n; j++){
+                int c_idx = i*n+j; // row-major indexing: "C[i,j] = C[c_idx]"
+                int b_idx = l*n+j; // row-major indexing: "B[l,j] = B[b_idx]"
+
+                C[c_idx] += A[a_idx]*B[b_idx];
+            }
+        }
+    }
+}
+
+
+void matmult_kmn(int m,int n,int k,double *A,double *B,double *C){
+    // Intialize C-matrix to 0.
+    for (int c_idx = 0; c_idx < m * n; c_idx++) {
+        C[c_idx] = 0;
+    }
+
+    // These two loops iterates over indexes of C: C[i,j]
+    for (int l=0; l<k; l++){
+        for (int i=0; i<m; i++){
+            int a_idx = i*k+l; // row-major indexing: "A[i,l] = A[a_idx]"
+
+            // This loop computes: C[i,j] = sum_l A[i,l]*B[l,j]
+            for (int j=0; j<n; j++){
+                int c_idx = i*n+j; // row-major indexing: "C[i,j] = C[c_idx]"
+                int b_idx = l*n+j; // row-major indexing: "B[l,j] = B[b_idx]"
+
+                C[c_idx] += A[a_idx]*B[b_idx];
+            }
+        }
+    }
+}
+
+
+void matmult_knm(int m,int n,int k,double *A,double *B,double *C){
+    // Intialize C-matrix to 0.
+    for (int c_idx = 0; c_idx < m * n; c_idx++) {
+        C[c_idx] = 0;
+    }
+
+    // These two loops iterates over indexes of C: C[i,j]
+    for (int l=0; l<k; l++){
+        for (int j=0; j<n; j++){
+            int b_idx = l*n+j; // row-major indexing: "B[l,j] = B[b_idx]"
+
+            // This loop computes: C[i,j] = sum_l A[i,l]*B[l,j]
+            for (int i=0; i<m; i++){
+                int c_idx = i*n+j; // row-major indexing: "C[i,j] = C[c_idx]"
+                int a_idx = i*k+l; // row-major indexing: "A[i,l] = A[a_idx]"
+
+                C[c_idx] += A[a_idx]*B[b_idx];
+            }
+        }
+    }
+}
+
+
+
+void matmult_nkm(int m,int n,int k,double *A,double *B,double *C){
+    // Intialize C-matrix to 0.
+    for (int c_idx = 0; c_idx < m * n; c_idx++) {
+        C[c_idx] = 0;
+    }
+
+    // These two loops iterates over indexes of C: C[i,j]
+    for (int j=0; j<n; j++){
+        for (int l=0; l<k; l++){
+            int b_idx = l*n+j; // row-major indexing: "B[l,j] = B[b_idx]"
+
+            // This loop computes: C[i,j] = sum_l A[i,l]*B[l,j]
+            for (int i=0; i<m; i++){
+                int c_idx = i*n+j; // row-major indexing: "C[i,j] = C[c_idx]"
+                int a_idx = i*k+l; // row-major indexing: "A[i,l] = A[a_idx]"
+
+                C[c_idx] += A[a_idx]*B[b_idx];
+            }
+        }
+    }
+}
+
+
+void matmult_nmk(int m,int n,int k,double *A,double *B,double *C){
+
+    // These two loops iterates over indexes of C: C[i,j]
+    for (int j=0; j<n; j++){
+        for (int i=0; i<m; i++){
+            int c_idx = i*n+j; // row-major indexing: "C[i,j] = C[c_idx]"
+            C[c_idx] = 0;
+
+            // This loop computes: C[i,j] = sum_l A[i,l]*B[l,j]
+            for (int l=0; l<k; l++){
+                int a_idx = i*k+l; // row-major indexing: "A[i,l] = A[a_idx]"
+                int b_idx = l*n+j; // row-major indexing: "B[l,j] = B[b_idx]"
+
+                C[c_idx] += A[a_idx]*B[b_idx];
+            }
+        }
+    }
+}
+
+
 
 /*                 printf("i = %d \n", i);
                 printf("j = %d \n", j);
