a1beb94b0521faa54ff675dab8b5d9a1053bc5b2
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2025-01-16 14:21:06 +0100

    feat: get the parallel region out

--- assignment2/jacobi.c
+++ assignment2/jacobi.c
@@ -1,41 +1,47 @@
 /* jacobi.c - Poisson problem in 3d
  * 
  */
-#include <math.h>
 #include <stddef.h>
 #include "jacobi.h"
 
 int solve_jacobi(double ***U_new, double ***U_old, double ***F, int N, int max_it, double threshold) {
-  int iter;
-
-  for (iter = 0; iter < max_it; iter++){
-    jacobi(U_new, U_old, F, N);
-    double ***tmp = U_old;
-    U_old = U_new;
-    U_new = tmp;
+#pragma omp parallel
+  {
+    for (int iter = 0; iter < max_it; iter++) {
+      jacobi(U_new, U_old, F, N);
+#pragma omp single
+      {
+        double ***tmp = U_old;
+        U_old = U_new;
+        U_new = tmp;
+      }
+// barrier (implied)
+    }
   }
-  return iter;
+
+  return max_it;
 }
 
-void jacobi(double *** U_new, double *** U_old, double *** F, int N) {
+void jacobi(double ***U_new, double ***U_old, double ***F, int N) {
 
   double scale = 1.0 / 6.0;
   double delta_squared = 2.0 / (N + 1);
   delta_squared = delta_squared * delta_squared;
 
-#pragma omp parallel for default(none) shared(U_new, U_old, F, N, scale, delta_squared) schedule(dynamic, 10)
-  for (size_t i = 1; i <= N ; i++) {
+#pragma omp for
+  for (size_t i = 1; i <= N; i++) {
     for (size_t j = 1; j <= N; j++) {
-      for (size_t k = 1; k <= N; k++) {                    
-        U_new[i][j][k] = scale * ( 
-          U_old[i-1][j][k] + 
-          U_old[i+1][j][k] + 
-          U_old[i][j-1][k] + 
-          U_old[i][j+1][k] + 
-          U_old[i][j][k-1] + 
-          U_old[i][j][k+1] + 
-          delta_squared * F[i][j][k]);
+      for (size_t k = 1; k <= N; k++) {
+        U_new[i][j][k] = scale * (
+            U_old[i - 1][j][k] +
+                U_old[i + 1][j][k] +
+                U_old[i][j - 1][k] +
+                U_old[i][j + 1][k] +
+                U_old[i][j][k - 1] +
+                U_old[i][j][k + 1] +
+                delta_squared * F[i][j][k]);
       }
     }
   }
+// barrier (implied)
 }
