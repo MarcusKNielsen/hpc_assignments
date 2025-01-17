16e301ea3a39523f4c1b0a638da449a37cfd6d11
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2025-01-17 11:14:00 +0100

    fix: for loop for repeated tries

--- assignment2/main.c
+++ assignment2/main.c
@@ -159,24 +159,21 @@ int main(int argc, char *argv[]) {
     perror("array f: allocation failed");
     exit(-1);
   }
-
   allocation_t += omp_get_wtime();
 
-  initialize_t -= omp_get_wtime();
+  for (int i = 0; i < repeated; i++) {
+    initialize_t -= omp_get_wtime();
 #pragma omp parallel
-  {
-    initialize_data(u, f, N);
-#pragma omp single
     {
+      initialize_data(u, f, N);
+#pragma omp single
+      {
 #ifdef _JACOBI
-      initialize_border(u2, N);
+        initialize_border(u2, N);
 #endif
-      initialize_t += omp_get_wtime();
-
-      compute_t -= omp_get_wtime();
-    }
-
-    for (int i = 0; i < repeated; i++) {
+        initialize_t += omp_get_wtime();
+        compute_t -= omp_get_wtime();
+      }
 #ifdef _JACOBI
       solve_jacobi(u2, u, f, N, iter_max, tolerance);
 #endif
@@ -184,8 +181,8 @@ int main(int argc, char *argv[]) {
       solve_gauss_seidel(u, f, N, iter_max);
 #endif
     }
+    compute_t += omp_get_wtime();
   }
-  compute_t += omp_get_wtime();
 
   printf("%s, %f, %f, %f, %ld, %d\n",
          type,
