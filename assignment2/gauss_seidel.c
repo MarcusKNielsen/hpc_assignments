faf958061df311a06d85d026f3a5306a3531ad73
Asta Rustad <asr017@bucknell.edu>
2025-01-17 09:21:13 +0100

    Fixed a bug in the OpenMP code for Gaus Seidel

--- assignment2/gauss_seidel.c
+++ assignment2/gauss_seidel.c
@@ -23,8 +23,6 @@ void gauss_seidel(double ***u, double ***f, int N) {
   delta_squared = delta_squared * delta_squared;
   double one_sixth = 1.0 / 6.0;
 
- #pragma omp parallel
- {
  #pragma omp parallel for schedule(static,1) ordered(2)
   for (int i  = 1; i < N + 1; i++) {
     for (int j = 1; j < N + 1; j++) {
@@ -35,5 +33,5 @@ void gauss_seidel(double ***u, double ***f, int N) {
      #pragma omp ordered depend(source)
     }
   }
- }
+ 
 }
