c68f48f3e022a79f231d5cbe0c03289436150ec8
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2025-01-22 11:06:49 +0100

    fix: linking

--- assignment3/matmul/mkn_mnk_offload.h
+++ assignment3/matmul/mkn_mnk_offload.h
@@ -5,8 +5,12 @@
 #ifndef MATMUL__MKN_OFFLOAD_H_
 #define MATMUL__MKN_OFFLOAD_H_
 
+extern "C" {
+
 void matmult_mkn_offload(int m, int n, int k, double *A, double *B, double *C);
 
 void matmult_mnk_offload(int m, int n, int k, double *A, double *B, double *C);
+
+}
 
 #endif //MATMUL__MKN_OFFLOAD_H_
