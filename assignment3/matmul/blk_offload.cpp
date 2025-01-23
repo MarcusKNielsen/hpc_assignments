fccf4827f3778de328ade16ba1a42afaeec09d23
iverykios <iliasver96@gmail.com>
2025-01-23 09:27:39 +0100

    bug fix. produces correct resutls for some cases

--- assignment3/matmul/blk_offload.cpp
+++ assignment3/matmul/blk_offload.cpp
@@ -36,7 +36,7 @@ void matmult_blk_offload(int m, int n, int k, double *A, double *B, double *C){
     distribute parallel for \
     map(to: A[0:m*k], B[0:k*n], m, n, k, BLK) map(tofrom: C[0:m*n]) \
     collapse(2)
-    for(int i = 0; i < BLK; i += BLK){
+    for(int i = 0; i < m; i += BLK){
         for(int j = 0; j < n; j++){
             if(i + BLK - 1 < m){
                 double sum[BLK] = {0};
@@ -50,7 +50,7 @@ void matmult_blk_offload(int m, int n, int k, double *A, double *B, double *C){
                 }
             }
             else {
-                for(int i1 = 0; i1 < m-i; i1++){
+                for(int i1 = 0; i1 < m - BLK; i1++){
                     double sum = 0;
                     for(int l = 0; l < k; l++){
                         sum += A[(i+i1)*k + l] * B[l * n + j];
