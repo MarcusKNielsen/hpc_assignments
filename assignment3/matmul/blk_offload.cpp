0dae1b9be52988bfefe0023b63ba008cc8085be6
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2025-01-23 19:12:58 +0100

    feat: profiling

--- assignment3/matmul/blk_offload.cpp
+++ assignment3/matmul/blk_offload.cpp
@@ -7,36 +7,37 @@
     _a < _b ? _a : _b;       \
 })
 
+#ifndef BLK
 #define BLK 35
+#endif
 
-void matmult_blk_offload(int m, int n, int k, double *A, double *B, double *C){
-    
-    #pragma omp target teams num_teams(m) thread_limit(64) \
+void matmult_blk_offload(int m, int n, int k, double *A, double *B, double *C) {
+
+#pragma omp target teams num_teams(m) thread_limit(64) \
     distribute parallel for \
     map(to: A[0:m*k], B[0:k*n]) map(tofrom: C[0:m*n]) \
     collapse(2)
-    for(int i = 0; i < m; i += BLK){
-        for(int j = 0; j < n; j++){
-            if(i + BLK - 1 < m){
-                double sum[BLK] = {0};
-                for(int l=0;l<k;l++){   
-                    for(int i1=0; i1 < BLK; i1++){
-                        sum[i1] += A[(i+i1)*k + l] * B[l * n + j];
-                    }
-                }
-                for (int i1=0; i1 < BLK; i1++){
-                    C[(i + i1) * n + j] = sum[i1];
-                }
-            }
-            else {
-                for(int i1 = 0; i1 < m - i; i1++){
-                    double sum = 0;
-                    for(int l = 0; l < k; l++){
-                        sum += A[(i+i1)*k + l] * B[l * n + j];
-                    } 
-                    C[(i + i1) * n + j] = sum;
-                }
-            }
+  for (int i = 0; i < m; i += BLK) {
+    for (int j = 0; j < n; j++) {
+      if (i + BLK - 1 < m) {
+        double sum[BLK] = {0};
+        for (int l = 0; l < k; l++) {
+          for (int i1 = 0; i1 < BLK; i1++) {
+            sum[i1] += A[(i + i1) * k + l] * B[l * n + j];
+          }
         }
+        for (int i1 = 0; i1 < BLK; i1++) {
+          C[(i + i1) * n + j] = sum[i1];
+        }
+      } else {
+        for (int i1 = 0; i1 < m - i; i1++) {
+          double sum = 0;
+          for (int l = 0; l < k; l++) {
+            sum += A[(i + i1) * k + l] * B[l * n + j];
+          }
+          C[(i + i1) * n + j] = sum;
+        }
+      }
     }
+  }
 }
--- assignment3/matmul/matmult_asy_offload.cpp
+++ assignment3/matmul/matmult_asy_offload.cpp
@@ -1,16 +1,16 @@
 #include "matmult_asy_offload.h"
 
-void matmult_asy_offload(int m, int n, int k, double *A, double *B, double *C) {
-#define SPLITS 2
-  int length = m / SPLITS;
-  int upper_A = k * length;
-  int upper_C = n * length;
+#ifndef SPLITS
+#define SPLITS 5
+#endif
 
+void matmult_asy_offload(int m, int n, int k, double *A, double *B, double *C) {
+  int length = m / SPLITS;
 #pragma omp target enter data map(alloc: A[0:m*k], B[0:k*n], C[0:m*n])
 #pragma omp target update to(B[0:k*n])
   for (int s = 0; s < SPLITS; ++s) {
     int lower = s * length;
-#pragma omp target update to(A[lower*k:upper_A])
+#pragma omp target update to(A[k*lower:k*length])
 
 #pragma omp target teams loop nowait \
         num_teams(length) thread_limit(32)
@@ -29,7 +29,7 @@ void matmult_asy_offload(int m, int n, int k, double *A, double *B, double *C) {
       }
 
     }
-#pragma omp target update from(C[lower*n:upper_C])
+#pragma omp target update from(C[n*lower:n*length])
   }
 #pragma omp taskwait
 #pragma omp target exit data map(release: A[0:m*k], B[0:k*n], C[0:m*n])
