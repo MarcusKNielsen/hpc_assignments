6bbffd6062ab085bce7a6c3ba53d8351ac26408c
iverykios <iliasver96@gmail.com>
2025-01-23 09:14:58 +0100

    big fixes,still doesnt produce correct results

--- assignment3/matmul/blk_offload.cpp
+++ assignment3/matmul/blk_offload.cpp
@@ -34,25 +34,26 @@ void matmult_blk_offload(int m, int n, int k, double *A, double *B, double *C){
     */
     #pragma omp target teams num_teams(m) thread_limit(64) \
     distribute parallel for \
-    map(to: A[0:m*k], B[0:k*n]) map(tofrom: C[0:m*n]) \
+    map(to: A[0:m*k], B[0:k*n], m, n, k, BLK) map(tofrom: C[0:m*n]) \
     collapse(2)
     for(int i = 0; i < BLK; i += BLK){
         for(int j = 0; j < n; j++){
             if(i + BLK - 1 < m){
                 double sum[BLK] = {0};
-                for(int i1 = 0; i1 < BLK; i1++){
-                    for(int l = 0; l < k; l++){
-                        sum[i1] += A[(i + i1) * k + l] * B[l * n + j];
-                    } 
-                    C[(i + i1) * n + j] = sum;
+                for(int l=0;l<k;l++){   
+                    for(int i1=0; i1 < BLK; i1++){
+                        sum[i1] += A[(i+i1)*k + l] * B[l * n + j];
+                    }
+                }
+                for (int i1=0; i1 < BLK; i1++){
+                    C[(i + i1) * n + j] = sum[i1];
                 }
             }
             else {
-                double sum[BLK] = {0};
-                int bound = m - BLK;
-                for(int i1 = 0; i1 < bound; i1++){
+                for(int i1 = 0; i1 < m-i; i1++){
+                    double sum = 0;
                     for(int l = 0; l < k; l++){
-                        sum[i1] += A[(i + i1) * k + l] * B[l * n + j];
+                        sum += A[(i+i1)*k + l] * B[l * n + j];
                     } 
                     C[(i + i1) * n + j] = sum;
                 }
