8d39745554fc452aceb0d30845191c30de5ed26a
iverykios <iliasver96@gmail.com>
2025-01-23 13:48:58 +0100

    fix bug for test m=1

--- assignment3/matmul/blk_offload.cpp
+++ assignment3/matmul/blk_offload.cpp
@@ -13,7 +13,7 @@ void matmult_blk_offload(int m, int n, int k, double *A, double *B, double *C){
     
     #pragma omp target teams num_teams(m) thread_limit(64) \
     distribute parallel for \
-    map(to: A[0:m*k], B[0:k*n], m, n, k, BLK) map(tofrom: C[0:m*n]) \
+    map(to: A[0:m*k], B[0:k*n]) map(tofrom: C[0:m*n]) \
     collapse(2)
     for(int i = 0; i < m; i += BLK){
         for(int j = 0; j < n; j++){
@@ -29,7 +29,7 @@ void matmult_blk_offload(int m, int n, int k, double *A, double *B, double *C){
                 }
             }
             else {
-                for(int i1 = 0; i1 < m - BLK; i1++){
+                for(int i1 = 0; i1 < m - i; i1++){
                     double sum = 0;
                     for(int l = 0; l < k; l++){
                         sum += A[(i+i1)*k + l] * B[l * n + j];
