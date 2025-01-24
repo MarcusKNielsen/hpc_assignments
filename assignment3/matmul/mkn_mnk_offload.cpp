97fadcf1120afae4864e8f4d9b1ed6035323f4c3
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2025-01-24 12:01:34 +0100

    fix: mkn mnk slight improvement

--- assignment3/matmul/mkn_mnk_offload.cpp
+++ assignment3/matmul/mkn_mnk_offload.cpp
@@ -5,12 +5,12 @@
 #include "mkn_mnk_offload.h"
 
 void matmult_mkn_offload(int m, int n, int k, double *A, double *B, double *C) {
-  // init c
+#pragma omp target teams num_teams(m) thread_limit(32) distribute parallel for map(tofrom: C[0:m*n])
   for (int c_idx = 0; c_idx < m * n; c_idx++) {
     C[c_idx] = 0;
   }
 
-#pragma omp target teams num_teams(m) thread_limit(64) distribute parallel for map(to: A[0:m*k], B[0:k*n]) map(tofrom: C[0:m*n])
+#pragma omp target teams num_teams(m) thread_limit(32) distribute parallel for map(to: A[0:m*k], B[0:k*n]) map(tofrom: C[0:m*n])
   for (int i = 0; i < m; i++) {
     for (int l = 0; l < k; l++) {
       for (int j = 0; j < n; j++) {
@@ -21,7 +21,7 @@ void matmult_mkn_offload(int m, int n, int k, double *A, double *B, double *C) {
 }
 
 void matmult_mnk_offload(int m, int n, int k, double *A, double *B, double *C) {
-#pragma omp target teams num_teams(m) thread_limit(64) distribute parallel for map(to: A[0:m*k], B[0:k*n]) map(tofrom: C[0:m*n])
+#pragma omp target teams num_teams(m) thread_limit(32) distribute parallel for map(to: A[0:m*k], B[0:k*n]) map(tofrom: C[0:m*n])
   for (int i = 0; i < m; i++) {
     for (int j = 0; j < n; j++) {
       double sum = 0;
